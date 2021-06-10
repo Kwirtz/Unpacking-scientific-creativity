56512283e38ba28f5c2ab3fa430da2bb937aaf52
Kevin Wirtz <kevin-w@hotmail.fr>
2021-06-10 13:51:53 +0200

    Update for parallel

--- Paper/create_our_indicator.py
+++ Paper/create_our_indicator.py
@@ -3,6 +3,8 @@ import pickle
 from package.graphs.community.Louvain import * 
 from package.graphs.community.Infomap import * 
 from package.graphs.community.OSLOM import * 
+from joblib import Parallel, delayed
+
 
 #%% Authors
 name2index = pickle.load(open("Paper/Data/a02_authorlist/name2index.p", "rb" ) )
@@ -12,22 +14,26 @@ time_window_post = 0
 
 
 
-def compute_novelty(year_range,variable,B):
+def compute_novelty(year,variable,B):
     name2index = pickle.load(open("Paper/Data/{}/name2index.p".format(variable),"rb") )
     n = len(name2index)
-    for year in year_range:
-        focal = pickle.load(open("Paper/Data/{}/{}.p".format(variable,year),"rb" ))
-        if time_window_pre != 0:
-            for past in range(year-time_window_pre,year,1):
-                focal +=  pickle.load(open("Paper/Data/{}/{}.p".format(past,variable),"rb" ))
-        if time_window_post != 0:
-            for future in range(year+1, year+time_window_post+1, 1):
-                focal += pickle.load(open("Paper/Data/{}/{}.p".format(future,variable),"rb" ))
-        focal = focal.tocoo()
-        edge_list = [(i,j,{"weight":v}) for i,j,v in zip(focal.row, focal.col, focal.data)]
-        g = nx.Graph(edge_list)
-    
-        Louvain = Louvain_based_indicator(g, n = n, year = year, variable = variable, B = B)
-        results = Louvain.get_indicator()
+    focal = pickle.load(open("Paper/Data/{}/{}.p".format(variable,year),"rb" ))
+    if time_window_pre != 0:
+        for past in range(year-time_window_pre,year,1):
+            focal +=  pickle.load(open("Paper/Data/{}/{}.p".format(past,variable),"rb" ))
+    if time_window_post != 0:
+        for future in range(year+1, year+time_window_post+1, 1):
+            focal += pickle.load(open("Paper/Data/{}/{}.p".format(future,variable),"rb" ))
+    focal = focal.tocoo()
+    edge_list = [(i,j,{"weight":v}) for i,j,v in zip(focal.row, focal.col, focal.data)]
+    g = nx.Graph(edge_list)
+
+    Louvain = Louvain_based_indicator(g, n = n, year = year, variable = variable, B = B)
+    results = Louvain.get_indicator()
 
 compute_novelty(year_range = range(1980,2020), variable = "a02_authorlist", B = 500)
+
+for year in range(1980,2020):
+    compute_novelty(year = year, variable = "a02_authorlist", B = 500)
+
+Parallel(n_jobs=3)(delayed(compute_novelty)(year = i, variable = "a02_authorlist", B = 500) for i in range(1980,2020))
