fb6bc7d9faececadd7b5453c3b7f20f64b208e3d
Kevin Wirtz <kevin-w@hotmail.fr>
2021-06-10 13:41:51 +0200

    create a function
    Still need to add joblib

--- Paper/create_our_indicator.py
+++ Paper/create_our_indicator.py
@@ -3,29 +3,31 @@ import pickle
 from package.graphs.community.Louvain import * 
 from package.graphs.community.Infomap import * 
 from package.graphs.community.OSLOM import * 
+
 #%% Authors
+name2index = pickle.load(open("Paper/Data/a02_authorlist/name2index.p", "rb" ) )
+n = len(name2index)
 time_window_pre = 0
 time_window_post = 0
 
 
-name2index = pickle.load(open("Data/Authors/name2index.p","rb") )
-index2name = pickle.load(open("Data/Authors/index2name.p","rb") )
-n = len(name2index)
 
+def compute_novelty(year_range,variable,B):
+    name2index = pickle.load(open("Paper/Data/{}/name2index.p".format(variable),"rb") )
+    n = len(name2index)
+    for year in year_range:
+        focal = pickle.load(open("Paper/Data/{}/{}.p".format(variable,year),"rb" ))
+        if time_window_pre != 0:
+            for past in range(year-time_window_pre,year,1):
+                focal +=  pickle.load(open("Paper/Data/{}/{}.p".format(past,variable),"rb" ))
+        if time_window_post != 0:
+            for future in range(year+1, year+time_window_post+1, 1):
+                focal += pickle.load(open("Paper/Data/{}/{}.p".format(future,variable),"rb" ))
+        focal = focal.tocoo()
+        edge_list = [(i,j,{"weight":v}) for i,j,v in zip(focal.row, focal.col, focal.data)]
+        g = nx.Graph(edge_list)
+    
+        Louvain = Louvain_based_indicator(g, n = n, year = year, variable = variable, B = B)
+        results = Louvain.get_indicator()
 
-year = 2000
-for year in range(1980,2021,1):
-    focal = pickle.load(open("Data/Authors/{}.p".format(year),"rb") )
-    if time_window_pre != 0:
-        for past in range(year-time_window_pre,year,1):
-            focal += pickle.load(open("Data/Authors/{}.p".format(past),"rb") )
-    if time_window_post != 0:
-        for past in range(year+1, year+time_window_post+1, 1):
-            focal += pickle.load(open("Data/Authors/{}.p".format(past),"rb") )
-    focal = focal.tocoo()
-    edge_list = [(i,j,{"weight":v}) for i,j,v in zip(focal.row, focal.col, focal.data)]
-    g = nx.Graph(edge_list)
-
-Louvain = Louvain_based_indicator(g, B = 50)
-results = Louvain.get_indicator()
-
+compute_novelty(year_range = range(1980,2020), variable = "a02_authorlist", B = 500)
