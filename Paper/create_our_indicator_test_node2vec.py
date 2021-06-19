fe5bdc9e4872b0026d237dcbf317ea144c5d67ce
Kevin Wirtz <kevin-w@hotmail.fr>
2021-06-19 10:09:34 +0200

    Update create_our_indicator_test_node2vec.py

--- Paper/create_our_indicator_test_node2vec.py
+++ Paper/create_our_indicator_test_node2vec.py
@@ -1,16 +1,30 @@
 #%%% link prediction
-#%% test node2vec
 
-import torch_geometric
-import torch
-import pickle
 import networkx as nx
+from node2vec import Node2Vec
+import pickle
 
-variable = "a02_authorlist"
-year = 1980
+# Create a graph
+graph = nx.fast_gnp_random_graph(n=100, p=0.5)
 
-focal = pickle.load(open("Paper/Data/{}/{}.p".format(variable,year),"rb" ))
-focal = focal.tocoo()
-focal = [(i,j,{"weight":v}) for i,j,v in zip(focal.row, focal.col, focal.data)]
-focal = nx.Graph(focal)
-data = torch_geometric.utils.from_networkx(focal)
+# Precompute probabilities and generate walks - **ON WINDOWS ONLY WORKS WITH workers=1**
+node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=1)  # Use temp_folder for big graphs
+
+# Embed nodes
+model = node2vec.fit(window=10, min_count=1, batch_words=4)  # Any keywords acceptable by gensim.Word2Vec can be passed, `dimensions` and `workers` are automatically passed (from the Node2Vec constructor)
+
+# Look for most similar nodes
+model.wv.most_similar('2')  # Output node names are always strings
+
+# Save embeddings for later use
+model.wv.save_word2vec_format("test")
+
+# Save model for later use
+model.save("model")
+
+
+with open("test","rb") as f:
+    test = pickle.load(f)
+    
+len(model.wv['2'])
+graph.nodes(data=True)
