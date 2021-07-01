72df4e8ad614e17e30757bacd8cfce76c73b6c9b
Kevin Wirtz <kevin-w@hotmail.fr>
2021-07-01 15:40:47 +0200

    Update clean_date.py

--- package/cleaner/clean_date.py
+++ package/cleaner/clean_date.py
@@ -1,7 +1,8 @@
 import pymongo
 import yaml
 import tqdm
-
+import time
+import datetime
 
 with open("mongo_config.yaml", "r") as infile:
     pars = yaml.safe_load(infile)['PC_Kevin']
@@ -13,31 +14,29 @@ db = client['pkg']
 collection = db['articles']
 
 
-def create_year(collection,year_month = False, index = False):
+def create_year(collection, index = False):
 
     docs = collection.find({})
     
     for doc in tqdm.tqdm(docs):
         try:
             date = doc['DateCompleted'].split('-')
-            if year_month == False:
-                doc_infos = {"year": int(date[0])}
-            else:
-                year = date[0]
-                month = date[1]
-                doc_infos = {"yearmonth": int(year+month)}               
+            year = date[0]
+            month = date[1]
+            day = date[2]
+            s = day + "/" + month + "/" + year
+            unix = time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())
+            doc_infos = {"year": int(date[0]),"yearmonth": int(year+month), "unix":unix}           
             query = {'PMID':int(doc['PMID'])}
             newvalues = {'$set':doc_infos}
             collection.update_one(query,newvalues)
         except:
             pass
-    if index == True:
-        if year_month == False:    
-            collection.create_index([ ("year",1) ])
-        else:
-            collection.create_index([ ("yearmonth",1) ])
+    if index == True:  
+        collection.create_index([ ("year",1) ])
+        collection.create_index([ ("yearmonth",1) ])
 
 
 create_year(collection)
-create_year(collection,year_month = True)
+
 
