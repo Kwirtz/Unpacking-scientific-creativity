0e2057b3fdd3648623f43ccb46dca896115c8b32
Kevin Wirtz <kevin-w@hotmail.fr>
2021-09-15 19:04:01 +0200

    year frr

--- Paper/Run_indicators/3_run_cooc.py
+++ Paper/Run_indicators/3_run_cooc.py
@@ -9,7 +9,7 @@ from novelpy import create_cooc
 test = create_cooc(client_name = pars['client_name'],
                    db_name = "PKG",
                    collection_name = "articles_test",
-                   year_var="Journal_JournalIssue_PubDate_Year",
+                   year_var="year",
                    var = "c04_referencelist",
                    sub_var = "item",
                    weighted_network = True,self_loop = True)
@@ -18,7 +18,7 @@ test = create_cooc(client_name = pars['client_name'],
 test = create_cooc(client_name = pars['client_name'],
                    db_name = "PKG",
                    collection_name = "articles_test", 
-                   year_var="Journal_JournalIssue_PubDate_Year",
+                   year_var="year",
                    var = "c04_referencelist",
                    sub_var = "item",
                    weighted_network = False,self_loop = False)
@@ -27,7 +27,7 @@ test.main()
 # test = create_cooc(client_name = pars['client_name'],
 #                    db_name = "PKG",
 #                    collection_name = "articles", 
-#                    year_var="Journal_JournalIssue_PubDate_Year",
+#                    year_var="year",
 #                    var = "Mesh_year_category",
 #                    sub_var = "descUI",
 #                    weighted_network = False, self_loop = False)
@@ -36,7 +36,7 @@ test.main()
 # test = create_cooc(client_name = pars['client_name'],
 #                    db_name = "PKG",
 #                    collection_name = "articles", 
-#                    year_var="Journal_JournalIssue_PubDate_Year",
+#                    year_var="year",
 #                    var = "Mesh_year_category",
 #                    sub_var = "descUI",
 #                    weighted_network = True, self_loop = True)
