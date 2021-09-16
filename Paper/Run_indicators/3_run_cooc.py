04cd05ee0ce6e8b7bbf70d94977611aa3af17020
Kevin Wirtz <kevin-w@hotmail.fr>
2021-09-16 18:26:04 +0200

    Update 3_run_cooc.py

--- Paper/Run_indicators/3_run_cooc.py
+++ Paper/Run_indicators/3_run_cooc.py
@@ -30,8 +30,8 @@ ref_mesh = create_cooc(client_name = 'mongodb://localhost:27017',
                     db_name = "pkg",
                     collection_name = "articles", 
                     year_var="year",
-                    var = "Mesh_year_category",
-                    sub_var = "descUI",
+                    var = "a06_meshheadinglist",
+                    sub_var = "DescriptorName_UI",
                     weighted_network = False, self_loop = False)
 
 ref_mesh.main()
@@ -40,8 +40,8 @@ ref_mesh = create_cooc(client_name = 'mongodb://localhost:27017',
                     db_name = "PKG",
                     collection_name = "articles", 
                     year_var="year",
-                    var = "Mesh_year_category",
-                    sub_var = "descUI",
+                    var = "a06_meshheadinglist",
+                    sub_var = "DescriptorName_UI",
                     weighted_network = True, self_loop = True)
 
 #ref_mesh.main()
