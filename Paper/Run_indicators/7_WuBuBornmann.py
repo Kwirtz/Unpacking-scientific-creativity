1e994f0593b27bf7027626c24b4547192c725f67
Kevin Wirtz <kevin-w@hotmail.fr>
2022-05-30 16:58:39 +0200

    Update 7_WuBuBornmann.py

--- Paper/Run_indicators/7_WuBuBornmann.py
+++ Paper/Run_indicators/7_WuBuBornmann.py
@@ -1,40 +1,14 @@
 import novelpy
-from joblib import Parallel, delayed
-import tqdm
-import yaml
-import argparse
 
-parser = argparse.ArgumentParser(description='compute abstract and title centroid, var = pmid and chunksize')
 
-parser.add_argument('-year')
-args = parser.parse_args()
+focal_year = 2000
 
-with open(r"C:\Users\Beta\Documents\GitHub\Taxonomy-of-novelty\mongo_config.yaml", "r") as infile:
-    pars = yaml.safe_load(infile)['PC_BETA']
-    
-for year in range(2012,2016):
-    companion = novelpy.utils.run_indicator_tools.create_output(client_name = 'mongodb://localhost:27017', 
-                   db_name =  'pkg',
-                   collection_name = 'articles',
-                   var = 'refs_pmid_wos',
-                   var_id = 'PMID',
-                   var_year = 'year',
-                   indicator = "disruptiveness",
-                   focal_year = focal_year)
-    companion.get_item_paper()
+disruptiveness = novelpy.Disruptiveness(
+    collection_name = 'Citation_net',
+    focal_year = focal_year,
+    id_variable = 'PMID',
+    refs_list_variable ='refs_pmid_wos',
+    year_variable = 'year')
 
-    disruptiveness = novelpy.Disruptiveness(focal_year = year,
-                                    var_id = 'PMID',
-                                    var_refs_list ='refs_pmid_wos',
-                                    var_year = 'year')
-    
-    Parallel(n_jobs=30)(
-        delayed(disruptiveness.compute_scores)(
-            focal_paper_id = idx,
-            focal_paper_refs = data.current_items[idx],
-            tomongo = True,
-            client_name = pars['client_name'], 
-            db_name = pars['db_name'],
-            collection_name = 'citation_data',
-            collection2update = 'citation_data') 
-        for idx in tqdm.tqdm(list(data.current_items)))
+disruptiveness.get_indicators(parallel = False)
+
