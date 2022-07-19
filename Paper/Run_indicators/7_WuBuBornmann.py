0abb58c231fca09217087adbb0364c1c53dc3a58
PPelletier <35504815+P-Pelletier@users.noreply.github.com>
2022-07-19 18:22:16 +0200

    up variables creation

--- Paper/Run_indicators/7_WuBuBornmann.py
+++ Paper/Run_indicators/7_WuBuBornmann.py
@@ -1,5 +1,6 @@
 import novelpy
 
+<<<<<<< Updated upstream
 
 focal_year = 2000
 
@@ -11,4 +12,22 @@ disruptiveness = novelpy.Disruptiveness(
     year_variable = 'year')
 
 disruptiveness.get_indicators(parallel = False)
+=======
+#parser = argparse.ArgumentParser(description='compute abstract and title centroid, var = pmid and chunksize')
+
+#parser.add_argument('-year')
+#args = parser.parse_args()
+
+    
+for year in range(2012,2016):
+    companion = novelpy.utils.run_indicator_tools.create_output(client_name = 'mongodb://Pierre:ilovebeta67@localhost:27017', 
+                   db_name =  'novelty_sample',
+                   collection_name = 'Citation_net_sample',
+                   var = 'refs_pmid_wos',
+                   var_id = 'PMID',
+                   var_year = 'year',
+                   indicator = "disruptiveness",
+                   focal_year = focal_year)
+    companion.get_item_paper()
+>>>>>>> Stashed changes
 
