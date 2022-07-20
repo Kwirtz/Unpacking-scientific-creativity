752f0a2a0df606e49872ce9e92a5d5c5e7a7e9fb
Kevin Wirtz <kevin-w@hotmail.fr>
2022-07-20 07:29:15 +0200

    Shibayama
    Clean shibayama run indicators

--- Paper/Run_indicators/9_Shibayama_PW.py
+++ Paper/Run_indicators/9_Shibayama_PW.py
@@ -1,45 +1,15 @@
 import novelpy
 import tqdm
-import yaml
-import argparse
 
-parser = argparse.ArgumentParser(description='compute abstract and title centroid, var = pmid and chunksize')
-
-parser.add_argument('-year')
-args = parser.parse_args()
-
-with open(r"C:\Users\Beta\Documents\GitHub\Taxonomy-of-novelty\mongo_config.yaml", "r") as infile:
-    pars = yaml.safe_load(infile)['PC_BETA']
-        
-for year in range(2012,2016):
-    companion = novelpy.utils.run_indicator_tools.create_output(
-      client_name = pars['client_name'], 
-      db_name =  'PKG',
-      collection_name = 'articles')
-
-    embedding = novelpy.Novelty_embedding(
-      var_id = 'PMID',
-      var_ref = 'refs_embedding',
-      var_aut_profile = 'authors_profiles',
-      var_year = 'year') 
-
-    docs = companion.collection.find()
-    for doc in tqdm.tqdm(docs):
-        embedding.Shibayama2021(
-          doc = doc,
-          entity = 'title_embedding')
-        embedding.Shibayama2021(
-          doc = doc,
-          entity = 'abstract_embedding')
-
-
-        embedding.Author_proximity(
-          doc = doc,
-          entity = 'title_profile',
-          windows_size = 10)
-        embedding.Author_proximity(
-          doc = doc,
-          entity = 'abs_profile',
-          windows_size = 10)
-
-        embedding.update_paper_values()
+for focal_year in tqdm.tqdm(range(2000,2011), desc = "Computing indicator for window of time"):
+    shibayama = novelpy.indicators.Shibayama2021(
+        client_name = 'mongodb://localhost:27017',
+		db_name = 'novelty',
+        collection_name = 'embedding',
+        id_variable = 'PMID',
+        year_variable = 'year',
+        ref_variable = 'refs_embedding',
+        entity = ['title_embedding','abstract_embedding'],
+        focal_year = focal_year)
+    
+    shibayama.get_indicator()
